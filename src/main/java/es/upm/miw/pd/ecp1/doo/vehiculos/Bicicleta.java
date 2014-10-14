30e9e96a529376a8eac10bf5624c99a30ae512fc
jackeline <jackeline@192.168.1.38>
2014-10-14 22:20:55 +0200

    Creacion de la vista publica de las clases: Coche, Bicicleta, moto, vehiculo, categoria, y vehiculoHandler

---
+++ src/main/java/es/upm/miw/pd/ecp1/doo/vehiculos/Bicicleta.java
@@ -0,0 +1,15 @@
+package es.upm.miw.pd.ecp1.doo.vehiculos;
+
+public class Bicicleta extends Vehiculo{
+
+	public Bicicleta(int id, String descripcion){	
+		super(id, descripcion);
+	}
+
+	@Override
+	public double precio(int dias) {
+		return 0.0;
+		
+	}
+	
+}
---
+++ src/main/java/es/upm/miw/pd/ecp1/doo/vehiculos/VehiculoHandler.java
@@ -0,0 +1,16 @@
+package es.upm.miw.pd.ecp1.doo.vehiculos;
+
+public class VehiculoHandler {
+
+	public void add(Vehiculo vehic) {        
+    }
+
+    public double precioVehiculo(int ident, int dias) {     	
+    	return 0.0;    	
+    }
+    
+    public String imprimirVehiculos() {
+        return "";
+    }
+	
+}
