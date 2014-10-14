61eb64529a2200c56eafc0da9d60dce843a9dbd9
jackeline <jackeline@192.168.1.38>
2014-10-14 16:09:19 +0200

    Cambiar el nombre de la clase “Addition” por “Summation”, e Incorporar la operación “Multiplication”

---
+++ src/main/java/es/upm/miw/pd/ecp1/doo/Summation.java
@@ -0,0 +1,28 @@
+package es.upm.miw.pd.ecp1.doo;
+
+public class Summation {
+    private final int operator1, operator2;
+
+    public Summation(final int operator1, final int operator2) {
+        this.operator1 = operator1;
+        this.operator2 = operator2;
+    }
+
+    public int getOperator1() {
+        return operator1;
+    }
+
+    public int getOperator2() {
+        return operator2;
+    }
+
+    public int sum() {
+        return this.operator1 + this.operator2;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + operator1 + "+" + operator2 + "]";
+    }
+
+}
